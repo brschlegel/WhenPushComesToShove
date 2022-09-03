a412426d7e507b495aeed6073d1583c57f52d4fa
Benjamin Schlegel (RIT Student) <brs1823@rit.edu>
2022-09-02 20:05:26 -0400

    Added in basic health system

--- WhenPushComesToShove/Assets/3-Scripts/Player Scripts/PlayerHitHandler.cs
+++ WhenPushComesToShove/Assets/3-Scripts/Player Scripts/PlayerHitHandler.cs
@@ -6,10 +6,14 @@ public class PlayerHitHandler : HitHandler
 {
   [SerializeField]
   private KnockbackReciever kbReciever;
+  [SerializeField]
+  private Health health;
   public override void RecieveHit(HitEvent e)
   {
-    KnockbackData data = e.hitbox.knockbackData;
-    Vector2 direction =  e.hurtbox.transform.position-e.hitbox.transform.position;
-    kbReciever.TakeKnockback(data.strength, direction.normalized);
+      KnockbackData kbData = e.hitbox.knockbackData;
+      AttackData attackData = e.hitbox.attackData;
+      Vector2 direction = e.hurtbox.transform.position - e.hitbox.transform.position;
+      kbReciever.TakeKnockback(kbData.strength, direction.normalized);
+      health.TakeDamage(attackData.damage);
   }
 }
