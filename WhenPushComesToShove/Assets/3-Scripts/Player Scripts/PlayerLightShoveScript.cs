b70d7788262b23a98d0355209223d1af7cc02c35
Neyree123 <treptaudestiny@gmail.com>
2022-09-06 15:20:20 -0400

    Base Loot and Inventory Framework

--- WhenPushComesToShove/Assets/3-Scripts/Player Scripts/PlayerLightShoveScript.cs
+++ WhenPushComesToShove/Assets/3-Scripts/Player Scripts/PlayerLightShoveScript.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerLightShoveScript : MonoBehaviour
 {
+    public Action onLightShove;
+
     [SerializeField] private GameObject hitbox;
     [SerializeField] private float cooldown = .5f;
 
     public void Start()
     {
         hitbox.SetActive(false);
+
+        EnableBaseLightShove();
     }
 
     /// <summary>
     /// A function called in input handler to activate the hitbox for the light shove
     /// </summary>
-    public void OnLightShove()
+    public void EnableShoveBaseHitbox()
     {
         hitbox.SetActive(true);
         StartCoroutine(HitboxCooldown());
@@ -29,5 +34,15 @@ public class PlayerLightShoveScript : MonoBehaviour
     {
         yield return new WaitForSeconds(cooldown);
         hitbox.SetActive(false);
+    }
+
+    public void EnableBaseLightShove()
+    {
+        onLightShove += EnableShoveBaseHitbox;
+    }
+
+    public void DisableBaseLightShove()
+    {
+        onLightShove = null;
     }
 }
