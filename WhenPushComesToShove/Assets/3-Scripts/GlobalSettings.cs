4083aae0b9a12575ec0a5d227736b3725b75bf3a
Benjamin Schlegel (RIT Student) <brs1823@rit.edu>
2022-09-10 12:19:56 -0400

    Added damage on bounce

--- WhenPushComesToShove/Assets/3-Scripts/GlobalSettings.cs
+++ WhenPushComesToShove/Assets/3-Scripts/GlobalSettings.cs
@@ -5,4 +5,6 @@ using UnityEngine;
 public static class GlobalSettings
 {
    public readonly static float frictionCoeff = .6f;
+   public readonly static float wallDamageCoeff = 1;
+   public readonly static float wallDamageCap = 20.0f;
 }
--- WhenPushComesToShove/Assets/3-Scripts/Utilities/Physics/WallData.cs
+++ WhenPushComesToShove/Assets/3-Scripts/Utilities/Physics/WallData.cs
@@ -8,4 +8,6 @@ public class WallData : MonoBehaviour
     [Tooltip("How much an object will bounce off of this wall (0 for no bounce, 1 for no speed lost")]
     [Range(0,1)]
     public float elasticity;
+    [Tooltip("How much to multiply the velocity-based damage multiplier")]
+    public float damageMultiplier;
 }
