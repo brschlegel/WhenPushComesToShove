ed9adb30f25fd357affcd16bb09226e6b5249c8b
Benjamin Schlegel (RIT Student) <brs1823@rit.edu>
2022-09-13 20:55:32 -0400

    Added in PVP in arena

--- WhenPushComesToShove/Assets/3-Scripts/LootScripts/LootDataScripts/FriendEnder.cs
+++ WhenPushComesToShove/Assets/3-Scripts/LootScripts/LootDataScripts/FriendEnder.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class FriendEnder : LootData
 {
+    
     public override void Action()
     {
-        player.GetComponentInChildren<DamageHitHandler>().tagsToIgnore.Remove("Shove");
+        playerRef.GetComponentInChildren<DamageHitHandler>().tagsToIgnore.Remove("Shove");
     }
 }
