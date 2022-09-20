6ee9312e4064f562ca26239ad16f87ecca1158e1
Mason Hayes (RIT Student) <mbh9947@rit.edu>
2022-09-20 13:06:05 -0400

    Got the loot resetting at the end of the game working

--- WhenPushComesToShove/Assets/3-Scripts/Player Scripts/PlayerInventory.cs
+++ WhenPushComesToShove/Assets/3-Scripts/Player Scripts/PlayerInventory.cs
@@ -15,12 +15,25 @@ public class PlayerInventory : MonoBehaviour
     private PlayerInputHandler inputHandler;
     private List<LootData> inRangeLoot;
 
+    private LootManager lootManager;
+    private void OnEnable()
+    {
+        LevelManager.onEndGame += ResetLoot;
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.onEndGame -= ResetLoot;
+    }
+
     public void Awake()
     {
         inputHandler = transform.parent.GetComponentInChildren<PlayerInputHandler>();
 
         passiveLoot = new List<LootData>();
         inRangeLoot = new List<LootData>();
+
+        lootManager = GameObject.Find("LootManager").GetComponent<LootManager>();
     }
 
     public void EquipItem(LootData loot) //TODO Prompt player for loot switch in some way
@@ -45,6 +58,7 @@ public class PlayerInventory : MonoBehaviour
 
                     lightShoveScript.onLightShove += loot.Action;
                     loot.gameObject.SetActive(false);
+                    lootManager.droppedLoot.Remove(loot);
                 }
                 //Otherwise assign equiping the loot to the select action
                 else
@@ -68,6 +82,7 @@ public class PlayerInventory : MonoBehaviour
 
                     heavyShoveScript.onHeavyShove += loot.Action;
                     loot.gameObject.SetActive(false);
+                    lootManager.droppedLoot.Remove(loot);
                 }
                 //Otherwise assign equiping the loot to the select action
                 else
@@ -81,7 +96,7 @@ public class PlayerInventory : MonoBehaviour
                 //Add the loot
                 passiveLoot.Add(loot);
                 loot.OnEquip(transform.parent);
-
+                lootManager.droppedLoot.Remove(loot);
                 //Trigger the Pasive Action
                 loot.Action();
 
@@ -109,18 +124,24 @@ public class PlayerInventory : MonoBehaviour
     /// Drops the current loot in the existing category
     /// </summary>
     /// <param name="type"></param>
-    public void DropLoot(LootData.LootType type)
+    public void DropLoot(LootData.LootType type, bool dropOnGround = true)
     {
         switch (type)
         {
+            
             case LootData.LootType.Light:
+                
                 lightShoveLoot.OnUnequip(transform.parent);
                 lightShoveScript.onLightShove = null;
                 lightShoveScript.EnableBaseLightShove();
 
-                lightShoveLoot.transform.position = transform.parent.position;
-                lightShoveLoot.gameObject.SetActive(true);
+                if (dropOnGround)
+                {
+                    lightShoveLoot.transform.position = transform.parent.position;
+                    lightShoveLoot.gameObject.SetActive(true);
+                }
 
+                lootManager.droppedLoot.Add(lightShoveLoot);
                 lightShoveLoot = null;
                 break;
             case LootData.LootType.Heavy:
@@ -128,9 +149,12 @@ public class PlayerInventory : MonoBehaviour
                 heavyShoveScript.onHeavyShove = null;
                 heavyShoveScript.EnableBaseHeavyShove();
 
-                heavyShoveLoot.transform.position = transform.parent.position;
-                heavyShoveLoot.gameObject.SetActive(true);
-
+                if (dropOnGround)
+                {
+                    heavyShoveLoot.transform.position = transform.parent.position;
+                    heavyShoveLoot.gameObject.SetActive(true);
+                }
+                lootManager.droppedLoot.Add(heavyShoveLoot);
                 heavyShoveLoot = null;
                 break;
             case LootData.LootType.Passive: //Not sure if we want players to drop passive loot
@@ -140,6 +164,24 @@ public class PlayerInventory : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Gets rid of all players loop
+    /// </summary>
+    void ResetLoot()
+    {
+        if(lightShoveLoot != null)
+            DropLoot(lightShoveLoot.lootType, false);
+
+        if(heavyShoveLoot != null)
+            DropLoot(heavyShoveLoot.lootType, false);
+
+        foreach(LootData loot in passiveLoot)
+        {
+            loot.OnUnequip(transform.parent);
+        }
+
+        passiveLoot.Clear();
+    }
     /// <summary>
     /// Removes the loot from being able to be picked up
     /// </summary>
