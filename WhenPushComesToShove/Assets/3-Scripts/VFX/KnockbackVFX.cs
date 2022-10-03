8debee5b9669c1aa284052fa51f81bbb25925a5b
Ben Schlegel <brschlegels@gmail.com>
2022-10-03 16:38:12 -0400

    Got vertical in

--- WhenPushComesToShove/Assets/3-Scripts/VFX/KnockbackVFX.cs
+++ WhenPushComesToShove/Assets/3-Scripts/VFX/KnockbackVFX.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class KnockbackVFX : MonoBehaviour
 {
+    [Header("Exclamation")]
     [SerializeField]
     private Transform exclamationTransform;
     [SerializeField]
     private ParticleSystem exclamationPS;
+
+    [Header("Wind Gust")]
     [SerializeField]
     private Transform gustTransform;
     [SerializeField]
     private ParticleSystem gustPS;
+    [SerializeField]
+    private Vector3 horizontalOffset;
+    [SerializeField]
+    private Vector3 verticalOffset;
     [SerializeField]
     private Material windGustMat;
     [SerializeField]
     private Texture horizontalSheet;
     [SerializeField]
     private Texture verticalSheet;
+
+    private ParticleSystem.ShapeModule gustShape;
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {   
+        gustShape = gustPS.shape;
     }
 
     // Update is called once per frame
@@ -43,18 +52,20 @@ public class KnockbackVFX : MonoBehaviour
         Vector2 compareTo = -xSign * Vector2.right;
         float angle = Vector2.Angle(compareTo, -direction);
 
+        //Horizontal
         if(angle <= 45)
         {
-            
             gustPS.transform.localScale = new Vector3(Mathf.Abs(gustPS.transform.localScale.x) * xSign,gustPS.transform.localScale.y, gustPS.transform.localScale.z ); 
             gustTransform.right = Vector2.right;
             windGustMat.mainTexture = horizontalSheet;
+            gustShape.position = horizontalOffset;
         }
         else 
         {
             gustPS.transform.localScale = new Vector3(gustPS.transform.localScale.x, Mathf.Abs(gustPS.transform.localScale.y) * ySign, gustPS.transform.localScale.z );
             gustTransform.right = Vector2.right;
             windGustMat.mainTexture = verticalSheet;
+            gustShape.position = verticalOffset;
         }
 
         // if(angle >= 90)
