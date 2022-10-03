95fb96dc2b4b37e89dd4ca0b76af07b1e435714a
Neyree123 <treptaudestiny@gmail.com>
2022-10-03 13:47:53 -0400

    Merge branch 'Dev' into Playtest-Fixes

--- WhenPushComesToShove/Assets/3-Scripts/Player Scripts/AnimationStates/PlayerLightShoveState.cs
+++ WhenPushComesToShove/Assets/3-Scripts/Player Scripts/AnimationStates/PlayerLightShoveState.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerLightShoveState : State
 {
+    [HideInInspector]
+    public ParticleSystem shoveExclamation;
     private void OnEnable()
     {
         anim.Play("Base Layer.AN_Player_LightShove");
+        //shoveExclamation.Play();
+        
     }
 
     // Update is called once per frame
