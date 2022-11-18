b4e25b0b23ea66e6d033ce8ed51a8369da7686a8
Neyree123 <treptaudestiny@gmail.com>
2022-11-18 14:09:14 -0500

    Soccer Level Fixes

--- WhenPushComesToShove/Assets/3-Scripts/MinigameLogic/MinigameLogic.cs
+++ WhenPushComesToShove/Assets/3-Scripts/MinigameLogic/MinigameLogic.cs
@@ -14,13 +14,21 @@ public abstract class MinigameLogic : MonoBehaviour
     protected BaseEndCondition endCondition;
     protected bool gameRunning;
 
+    [SerializeField] protected bool canPlayersTakeDamage = true;
+
 
     public virtual void Init()
     {
         startingUIDisplay.ShowDisplay();
+
+        if (!canPlayersTakeDamage)
+        {
+            UpdatePlayerInvulnurability(false);
+        }
+
         CoroutineManager.StartGlobalCoroutine(WaitToStartGame());
     }
-    
+
     public virtual void StartGame()
     {
         gameRunning = true;
@@ -34,13 +42,18 @@ public abstract class MinigameLogic : MonoBehaviour
     }
     public virtual void CleanUp()
     {
+        if (!canPlayersTakeDamage)
+        {
+            UpdatePlayerInvulnurability(true);
+        }
+
         endingUIDisplay.HideDisplay();
         LevelManager.onNewRoom.Invoke();
     }
 
     protected IEnumerator WaitToStartGame()
     {
-        if(startingUIDisplay != null)
+        if (startingUIDisplay != null)
         {
             yield return new WaitUntil(() => startingUIDisplay.isDone);
         }
@@ -49,10 +62,20 @@ public abstract class MinigameLogic : MonoBehaviour
 
     protected IEnumerator WaitToCleanUp()
     {
-        if(endingUIDisplay != null)
+        if (endingUIDisplay != null)
         {
             yield return new WaitUntil(() => endingUIDisplay.isDone);
         }
         CleanUp();
+    }
+
+    protected void UpdatePlayerInvulnurability(bool enabled)
+    {
+        GameState.damageEnabled = enabled;
+
+        foreach (Transform p in GameState.players)
+        {
+            p.GetComponentInChildren<HealthBar>().gameObject.SetActive(enabled);
+        }
     }
 }
