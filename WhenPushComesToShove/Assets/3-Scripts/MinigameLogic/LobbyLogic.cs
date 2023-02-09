d634a8e193a8c80f3cb97117fc674b2d55f94597
Destiny <treptaudestiny@gmail.com>
2023-02-09 13:19:12 -0500

    Added new sword

--- WhenPushComesToShove/Assets/3-Scripts/MinigameLogic/LobbyLogic.cs
+++ WhenPushComesToShove/Assets/3-Scripts/MinigameLogic/LobbyLogic.cs
@@ -5,4 +5,24 @@ using UnityEngine;
 public class LobbyLogic : MinigameLogic
 {
 
+    public void OnDisable()
+    {
+        if (GameState.players.Count > 0 && GameState.lastGameWinnerIndex != -1)
+        {
+            PlayerInputHandler handler = GameState.players[GameState.lastGameWinnerIndex].GetComponentInChildren<PlayerInputHandler>();
+
+            handler.sword.gameObject.SetActive(false);
+        }
+    }
+
+    public override void Init()
+    {
+        if (GameState.players.Count > 0 && GameState.lastGameWinnerIndex != -1)
+        {
+            PlayerInputHandler handler = GameState.players[GameState.lastGameWinnerIndex].GetComponentInChildren<PlayerInputHandler>();
+
+            handler.sword.gameObject.SetActive(true);
+            handler.sword.GetComponentInChildren<SpriteRenderer>().color = PlayerConfigManager.Instance.playerColors[handler.playerConfig.PlayerIndex];
+        }
+    }
 }
