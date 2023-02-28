3cfdde5a64cfca302abbd22cf986d4859eaf2901
Benjamin Schlegel (RIT Student) <brs1823@rit.edu>
2023-02-28 12:28:57 -0500

    moved prefabs and added velocity damage threshold

--- WhenPushComesToShove/Assets/3-Scripts/CombatScripts/VelocityBasedHitboxes/ProjectileHitbox.cs
+++ WhenPushComesToShove/Assets/3-Scripts/CombatScripts/VelocityBasedHitboxes/ProjectileHitbox.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [RequireComponent(typeof(AttackData))]
 public class ProjectileHitbox : MonoBehaviour
@@ -11,6 +12,8 @@ public class ProjectileHitbox : MonoBehaviour
     private Hitbox hitbox;
     [SerializeField]
     private float instanceDamageMultiplier = 1;
+
+    private List<Tuple<float, float>> velocityDamageThresholds;
     // Start is called before the first frame update
     void OnEnable()
     {
@@ -24,13 +27,17 @@ public class ProjectileHitbox : MonoBehaviour
     {
         data = GetComponent<AttackData>();
         hitbox = GetComponent<Hitbox>();
+        velocityDamageThresholds = new List<Tuple<float, float>>();
+        velocityDamageThresholds.Add(new Tuple<float, float>(4, 7));
+        velocityDamageThresholds.Add(new Tuple<float, float>(18,15));
+        velocityDamageThresholds.Add(new Tuple<float, float>(25,25));
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        data.damage = instanceDamageMultiplier * Mathf.Clamp(pMode.Mass * pMode.Velocity.magnitude * GlobalSettings.velocityDamageCoeff , 0, GlobalSettings.velocityDamageCap);
+        data.damage = instanceDamageMultiplier * Mathf.Clamp(pMode.Mass * GetThresholdValue(pMode.Velocity.magnitude) * GlobalSettings.velocityDamageCoeff , 0, GlobalSettings.velocityDamageCap);
+        Debug.Log(data.damage);
     }
 
     public List<GameObject> OwnersToIgnore
@@ -44,5 +51,19 @@ public class ProjectileHitbox : MonoBehaviour
             return hitbox.OwnersToIgnore;
     
         }
+    }
+
+    private float GetThresholdValue(float velocity)
+    {
+        float thresholdValue = 0;
+        for(int i = 0; i < velocityDamageThresholds.Count; i++)
+        {
+            if(pMode.Velocity.magnitude <= velocityDamageThresholds[i].Item1)
+            {
+                return thresholdValue;
+            }
+            thresholdValue = velocityDamageThresholds[i].Item2;
+        }
+        return thresholdValue;
     }
 }
