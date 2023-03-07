fe83b139889beacbbea62562654cdcd8cf6aa777
Destiny <treptaudestiny@gmail.com>
2023-03-07 14:26:34 -0500

    Ready Up UI fix

--- WhenPushComesToShove/Assets/3-Scripts/UIDisplays/ReadyUpBase.cs
+++ WhenPushComesToShove/Assets/3-Scripts/UIDisplays/ReadyUpBase.cs
@@ -140,6 +140,7 @@ public class ReadyUpBase : UIDisplay
     private void StartCounting()
     {
         readyParent.gameObject.SetActive(false);
+        vs2v2.gameObject.SetActive(false);
         countdown.countdownTime = countdownTime;
         countdown.gameObject.SetActive(true);
         countdown.onCountdownEnded += HideDisplay;
