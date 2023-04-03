e8b0669bd9e09b8b0aa4f92a38a916b9edf6000b
MBHayes00 <54604017+MBHayes00@users.noreply.github.com>
2023-04-03 12:58:05 -0400

    Changed to OnTriggerEnter

--- WhenPushComesToShove/Assets/3-Scripts/LevelSystems/EndConditions/PushDoorEndCondition.cs
+++ WhenPushComesToShove/Assets/3-Scripts/LevelSystems/EndConditions/PushDoorEndCondition.cs
@@ -73,7 +73,7 @@ public class PushDoorEndCondition : BaseEndCondition
         lightsToTurnOff.Clear();
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Shove")
         {
