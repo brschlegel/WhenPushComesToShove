5606f11ca20b0bcf1b5481b267b2d6e37a28721e
Destiny <treptaudestiny@gmail.com>
2023-04-13 13:11:43 -0400

    Added tags to ignore on the teleporter

--- WhenPushComesToShove/Assets/3-Scripts/Props/Teleporter.cs
+++ WhenPushComesToShove/Assets/3-Scripts/Props/Teleporter.cs
@@ -7,14 +7,20 @@ public class Teleporter : MonoBehaviour
     [SerializeField]
     private Vector2 destination;
 
+    public List<string> tagsToIgnore;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        foreach (string s in tagsToIgnore)
         {
-            //Ensures the object is teleporting along the z axis
-            collision.transform.position = new Vector3(destination.x, destination.y, collision.transform.position.z);
-        }        
+            if (collision.CompareTag(s))
+            {
+                return;
+            }
+        }
+
+        //Ensures the object is teleporting along the z axis
+        collision.transform.position = new Vector3(destination.x, destination.y, collision.transform.position.z);
     }
 
     private void OnDrawGizmos()
